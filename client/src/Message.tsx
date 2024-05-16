0d9ac57cfaf444a7f5529e3322506ad7f2f8ccce
Henry-W-1015 <82410964+Henry-W-1015@users.noreply.github.com>
2024-05-16 03:11:56 -0400

    Build front
    still learning

---
+++ client/src/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing
+function Message() {
+  const name = "Hang";
+
+  if (name) {
+    return <h1> Hello {name} </h1>;
+  } else {
+    return <h1> Hello World </h1>;
+  }
+}
+export default Message;
