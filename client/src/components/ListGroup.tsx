fa5742dadd91c855ff17ed2a6a90813cff67541b
Henry-W-1015 <82410964+Henry-W-1015@users.noreply.github.com>
2024-05-17 02:35:03 -0400

    made a testing file(unfinished)

--- client/src/components/ListGroup.tsx
+++ client/src/components/ListGroup.tsx
@@ -1,12 +1,23 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+  const clickHandler = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item active"
+            key={item}
+            onClick={clickHandler}
+          >
+            {item}
+          </li>
         ))}
         {/* <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
